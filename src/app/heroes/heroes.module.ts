04fe14aa2b6d2883f980688f9d78a3b720513d47
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-04-08 22:08:17 -0300

    implementación angular material, menu

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -6,6 +6,8 @@ import { HeroeComponent } from './pages/heroe/heroe.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { HeroesRoutingModule } from './heroes-routing.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from '../material/material.module';
 
 
 
@@ -17,6 +19,8 @@ import { HeroesRoutingModule } from './heroes-routing.module';
     ListadoComponent],
   imports: [
     CommonModule,
+    FlexLayoutModule,
+    MaterialModule,
     HeroesRoutingModule
   ]
 })
