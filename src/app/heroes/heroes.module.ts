34b842d9df2c729aa3c26e64c43440e3d1dcf2c9
Felipe Larraguibel <felipelarra.araya@gmail.com>
2021-04-15 21:15:38 -0300

    CRUD finalizado, falta agregar animaciones y resolver temas de validaciones en formulario y mejorar diseño

--- src/app/heroes/heroes.module.ts
+++ src/app/heroes/heroes.module.ts
@@ -11,6 +11,7 @@ import { MaterialModule } from '../material/material.module';
 import { HeroeTarjetaComponent } from './components/heroe-tarjeta/heroe-tarjeta.component';
 import { ImagenPipe } from './pipes/imagen.pipe';
 import { FormsModule } from '@angular/forms';
+import { ConfirmarComponent } from './components/confirmar/confirmar.component';
 
 
 
@@ -19,7 +20,7 @@ import { FormsModule } from '@angular/forms';
     BuscarComponent, 
     HeroeComponent, 
     HomeComponent, 
-    ListadoComponent, HeroeTarjetaComponent, ImagenPipe],
+    ListadoComponent, HeroeTarjetaComponent, ImagenPipe, ConfirmarComponent],
   imports: [
     CommonModule,
     FlexLayoutModule,
--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from "@angular/material/input";
 import { MatIconModule } from '@angular/material/icon';
 import { MatListModule } from '@angular/material/list';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+
 
 
 
@@ -19,12 +23,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
     MatAutocompleteModule,
     MatButtonModule,
     MatCardModule,
+    MatDialogModule,
     MatGridListModule,
     MatFormFieldModule,
     MatInputModule,
     MatIconModule,
     MatListModule,
+    MatSnackBarModule,
     MatProgressSpinnerModule,
+    MatSelectModule,
     MatSidenavModule,
     MatToolbarModule
   ]
